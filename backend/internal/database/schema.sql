8eebd0b93190afc1bb66d668c4dafe581329db78
JbanzCodingLabz <jacob.r.bananal@outlook.com>
2026-03-29 18:26:54 -0700

    Add products table schema

---
+++ backend/internal/database/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    image_url TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);  \   
